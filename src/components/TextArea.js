b6f13bf68d82f3036b799246de88da181817e22d
Isaac Kabucho <kabszac@gmail.com>
2023-02-13 16:04:12 +0300

    Made changes to  TextArea component

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -5,7 +5,7 @@ import TextError from './TextError';
 const TextArea = (props) => {
     const {label, name, ...rest} = props
     return (
-        <div>
+        <div className='form-control'>
             <label htmlFor={name}>{label}</label>
             <Field as='textarea' name={name} id={name} {...rest}/>
             <ErrorMessage name={name} component={TextError}/>
